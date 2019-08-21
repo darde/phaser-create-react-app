ff8444b456127b54b52184a860c0a25b7705ab6a
darde <pablodarde@gmail.com>
2019-08-21 20:29:39 -0300

    Game Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Phaser from 'phaser';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 200 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+  };
+
+  const game = new Phaser.Game(config);
+
+  function preload() {
+    this.load.setBaseURL('http://localhost:3000/');
+    this.load.image('sky', './assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+      'assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+    );
+  }
+
+  function create() {
+    this.add.image(400, 300, 'sky');
+  }
+
+  function update() {
+  }
+
+  return (
+    <div />
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
